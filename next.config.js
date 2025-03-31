3898f0cbc49479daa207b698909f1f2546e54dda
Amirhossein Olyaei <amirholyaei@gmail.com>
2025-03-31 13:44:02 -0400

    Config: Add OpenAI DALL-E API domain to allowed image domains

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
+      },
     ],
   },
   env: {
